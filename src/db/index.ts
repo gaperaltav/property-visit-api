d5ca8a4e233a1b85776bdc649b258f7ba09b19a6
Gabriel Peralta <gaperaltav@gmail.com>
2024-10-30 18:36:54 -0400

    Moving DB connection Database URI into env variable

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 import debug from "debug";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DB_URI = "" } = process.env;
 
 const debugServer = debug("server:db");
 
 export const dbConnect = async () => {
   return mongoose
-    .connect("mongodb://localhost/property-visit")
+    .connect(DB_URI)
     .then(() => {
       debugServer("Connected to database");
     })
